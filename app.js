30858bd4a17b8ab30dc265f8416372e927d90231
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-03 15:24:43 -0700

    some functions and let states

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // import functions and grab DOM elements
+const ingredientsForm = document.getElementById('ingredients-form');
+const ingredientList = document.getElementById('ingredient-list');
+const finishedMeals = document.getElementById('meals');
+const ingredientInput = document.getElementById('ingredient-input');
+const quantityInput = document.getElementById('quantity-input');
+const servingSize = document.getElementById('serving-size');
+const ingredientButton = document.getElementById('add-ingredient-button');
+const removeButton = document.getElementById('remove');
+const nameOfMeal = document.getElementById('name-of-meal');
+const saveMealButton = document.getElementById('save-meal');
 
 // let state
+let ingredient = [];
+let meal = [];
 
 // set event listeners 
   // get user input
